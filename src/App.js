f5f7992277f9e052ef6466ecd7e737e363b1c99b
aravindmv97 <aravindmv97@gmail.com>
2019-08-02 09:10:59 +0530

    Header

--- src/App.js
+++ src/App.js
@@ -2,9 +2,12 @@ import React, { Fragment } from 'react';
 import styled from 'styled-components';
 import WallPage from './Wall';
 import Header from './Header';
+import Login from './Login';
+import Profile from './Profile';
 
 const AppWrapper = styled.div`
   padding: 20px;
+  height: 100%;
 `
 
 function App() {
@@ -12,7 +15,7 @@ function App() {
     <Fragment>
       <Header />
       <AppWrapper>
-        <WallPage />
+        <Profile />
       </AppWrapper>
     </Fragment>
   );
