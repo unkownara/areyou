2c806a6dcfcb60b40d6791e678d5df7ff8c32b84
aravindmv97 <aravindmv97@gmail.com>
2019-08-03 11:18:11 +0530

    Login and Signup validations

--- src/Login.js
+++ src/Login.js
@@ -1,15 +1,15 @@
-import React, { useState } from 'react';
-import styled from 'styled-components';
-import {useInput} from "./hooks";
-import {user_info_url} from './backend/Apis';
+import React, { useState, useEffect, useRef } from 'react';
+import styled, { keyframes } from 'styled-components';
+import cookie from "react-cookies";
 
+import { useInput } from "./hooks";
+import { user_info_url } from './backend/Apis';
 import SkipToAnswers from './SkipToAnswers';
 import history from "./history";
+import { makeid } from "./Generics";
 
 import ShowEye from './eye.png';
 import HideEye from './eyecross.png';
-import cookie from "react-cookies";
-import {makeid} from "./Generics";
 
 const LoginWrapper = styled.div`
   display: flex;
@@ -51,10 +51,12 @@ const InputWrapper = styled.div`
     flex-direction: column;
 `
 
+const InputContainer = styled.div``
+
 const Input = styled.input`
     width: 350px;
     height: 50px;
-    color: gray;
+    color: #000;
     border-radius: 7px;
     font-size: 18px;
     font-weight: 500;
@@ -71,16 +73,29 @@ const Input = styled.input`
 
     &:focus{
         outline: 0;
+        border: 1px solid #09198A;
     }
 
     @media(max-width: 700px){
+        min-width: 300px;
         width: 100%;
     }
 `
 
+const ErrorLabel = styled.span`
+    z-index: 999;
+    color: red;
+    font-size: 11px;
+    font-weight: 500;
+    background: #fff;
+    position: absolute;
+    padding: 0 5px;
+    margin: ${props => props.margin || '0'};
+`
+
 const Button = styled.div`
     background: #09198A;
-    height: 40px;
+    height: ${props => props.login ? '60px' : '40px'};
     vertical-align: middle;
     line-height: 40px;
     width: 300px;
@@ -91,10 +106,44 @@ const Button = styled.div`
     font-weight: bold;
     margin: 20px auto;
     cursor: pointer;
+    pointer-events: ${props => props.disabled ? 'none' : 'auto'};
 
     @media(max-width: 700px){
         cursor: default;
     }
+`
+
+
+const LoaderLine = keyframes`
+    0% {
+        margin-left: 0px;
+        width: 10px;
+    }
+
+    25% {
+        width: 30px;
+    }
+
+    50% {
+        width: 50px;
+    }
+
+    75% {
+        width: 30px;
+    }
+
+    100% {
+        margin-left: 260px;
+        width: 10px;
+    }
+`
+
+const LineLoader = styled.div`
+    width: 10px;
+    height: 5px;
+    border-radius: 30px;
+    background: white;
+    animation: ${LoaderLine} .7s alternate infinite ease-in-out;
 `
 
 const SignUpRedirect = styled.div`
@@ -103,8 +152,28 @@ const SignUpRedirect = styled.div`
     font-size: 14px;
 
     &>span {
+        cursor: pointer;
         color: #09198A;
         font-weight: bold;
+    }
+
+    &:hover{
+        &>span {
+            text-decoration: underline;
+        }
+    }
+
+
+    @media(max-width: 700px){
+        &>span {
+            cursor: default;
+        }
+
+        &:hover{
+            &>span {
+                text-decoration: none;
+            }
+        }
     }
 `
 
@@ -135,95 +204,146 @@ function Login() {
     const email = useInput('');
     const password = useInput('');
     const [showPass, setShowPass] = useState(false);
-    const [errorMsg, setErrorMsg] = useState('');
+    const [emailErrorMsg, setEmailErrorMsg] = useState('');
+    const [passwordErrorMsg, setPasswordErrorMsg] = useState('');
+    const [verifyingCredentials, setVerifyingCredentials] = useState(false);
+    const passswordRef = useRef(null);
 
-    const onSubmit = () => {
-        import('./backend/ApiRequests').then(obj => {
-            let params = {
-                userId: email.value,
-                password: password.value
-            };
-            obj.getApiRequestCall(user_info_url, params, function(response) {
-                try {
-                    if (response && response.data && response.data.Items && response.data.Items.length > 0) {
-                        setErrorMsg('');
-                        cookie.save('__u_id__', email.value);
-                        history.push({
-                            pathname: '/',
-                            search: `?u_id=${makeid(6)}`,
-                            state: {detail: response.data.Items[0]}
-                        });
-                    } else if(response.data === "incorrect password" || response.data === "Email doesn't exists" ){
-                        setErrorMsg(response.data);
+    function ValidateEmailAndPassword() {
+
+        let errFlag = false;
+
+        let regex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+        if (email.value.length !== 0 && email.value !== undefined && email.value !== '') {
+            if (!regex.test(email.value)) {
+                setEmailErrorMsg('Invalid Email')
+                errFlag = true
+            } else {
+                errFlag = false
+            }
+        } else {
+            setEmailErrorMsg('Required');
+            errFlag = true
+        }
+
+        if (password.value === '' || password.value.length === 0 || password.value === undefined) {
+            setPasswordErrorMsg('Required');
+            errFlag = true
+        } else {
+            errFlag = false
+        }
+
+        return errFlag
+    }
+
+    function enterPressed(e) {
+        let code = e.keyCode || e.which;
+        if (code === 13) {
+            LoginUser();
+        }
+    }
+
+    const LoginUser = () => {
+        if (!ValidateEmailAndPassword()) {
+            setVerifyingCredentials(true);
+            import('./backend/ApiRequests').then(obj => {
+                let params = {
+                    userId: email.value,
+                    password: password.value
+                };
+                obj.getApiRequestCall(user_info_url, params, function (response) {
+                    try {
+                        if (response && response.data && response.data.Items && response.data.Items.length > 0) {
+                            setEmailErrorMsg('');
+                            cookie.save('__u_id__', email.value);
+                            history.push({
+                                pathname: '/',
+                                search: `?u_id=${makeid(6)}`,
+                                state: { detail: response.data.Items[0] }
+                            });
+                            setVerifyingCredentials(false);
+                        } else if (response.data === "incorrect password" || response.data === "Email doesn't exists") {
+                            setVerifyingCredentials(false);
+                            setEmailErrorMsg(response.data);
+                        }
+                    } catch (e) {
+                        setVerifyingCredentials(false);
+                        console.log('oops something went wrong');
                     }
-                } catch (e) {
-                    console.log('oops something went wrong');
-                }
+                })
             })
-        })
+        } else {
+            console.log('Fix errors');
+            setVerifyingCredentials(false);
+        }
     };
 
     const signUpRedirect = () => {
         history.push('/signup');
     };
-    
-
-    // function ValidateEmail(email) {
-    //     setVerifyingEmail(false);
-
-    //     var regex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-
-    //     if (email.length !== 0 && email !== undefined && email !== '') {
-    //         if (!regex.test(email)) {
-    //             setErrorMsg('Invalid Email')
-    //             return false;
-    //         } else {
-    //             return true;
-    //         }
-    //     } else {
-    //         setErrorMsg('Required');
-    //     }
-    // }
-
-    function enterPressed(e) {
-        let code = e.keyCode || e.which;
-        if (code === 13) {
-        }
-    }
 
     function togglePassword() {
-        setShowPass(!showPass)
+        setShowPass(!showPass);
+        passswordRef.current.focus();
     }
+
+    useEffect(() => {
+        setPasswordErrorMsg('')
+    }, [password.value]);
+
+    useEffect(() => {
+        setEmailErrorMsg('')
+    }, [email.value]);
 
     return (
         <LoginWrapper>
             <AppName>Are You ?</AppName>
             <LoginHeading>Login</LoginHeading>
             <InputWrapper>
-                <Input
-                    {...email}
-                    placeholder={'Email'}
-                />
+                <InputContainer>
+                    <Input
+                        {...email}
+                        placeholder={'Email'}
+                    />
+                    {
+                        emailErrorMsg.length ?
+                            <ErrorLabel
+                                margin={emailErrorMsg === 'Required' ? '-6.5px 0px 0px -65px' : emailErrorMsg === 'Invalid Email' ? '-6.5px 0px 0px -85px' : '-6.5px 0px 0px -117px'}>{emailErrorMsg}</ErrorLabel> : null
+                    }
+                </InputContainer>
                 <PasswordWrapper>
                     <Input
+                        ref={passswordRef}
                         {...password}
                         type={showPass ? 'text' : 'password'}
                         autoComplete="off"
                         placeholder="Password"
-                        style={{
-                            fontSize: password.value.length ? showPass ? '14px' : '22px' : '14px'
-                        }}
                         onKeyPress={(e) => enterPressed(e, 'password')}
                     />
                     {
-                        <PasswordHiderIcon src={showPass ? ShowEye : HideEye}
-                            className="passHideShowIcon" onClick={togglePassword}
-                            alt={'pass'} />
+                        !passwordErrorMsg.length ?
+                            <PasswordHiderIcon src={showPass ? ShowEye : HideEye}
+                                onClick={togglePassword}
+                                alt={'pass'} /> : null
+                    }
+                    {
+                        passwordErrorMsg.length ?
+                            <ErrorLabel
+                                margin={passwordErrorMsg === 'Required' ? '-6.5px 0px 0px -65px' : '-6.5px 0px 0px -130px'}>{passwordErrorMsg}</ErrorLabel> : null
                     }
                 </PasswordWrapper>
             </InputWrapper>
-            <Button onClick={onSubmit}>Let's Go</Button>
-            {errorMsg}
+            <Button
+                onClick={LoginUser}
+                disabled={verifyingCredentials}
+                login={verifyingCredentials}>
+                <span>{!verifyingCredentials ? "Let's Go" : 'Verifying ...'}</span>
+                {
+                    verifyingCredentials ?
+                        <LineLoader /> : null
+                }
+            </Button>
             <SignUpRedirect>Want to join us? <span onClick={signUpRedirect}>Sign Up</span></SignUpRedirect>
             <OR>or</OR>
             <SkipToAnswers />
